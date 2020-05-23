6691bfd4787fa22d5ce2698ff6e01a6bdca90497
Yao Josué <jyao2000@yahoo.fr>
2020-05-23 13:19:04 +0200

    schema

--- graphblog/schema.py
+++ graphblog/schema.py
@@ -1,17 +1,16 @@
 import graphene
 import graphql_jwt
 
-import tv.graphql.schema as t_schema
-import concoursApp.schema
+import blog.schema
 
 
-class Query(t_schema.Query, concoursApp.schema.Query, graphene.ObjectType):
+class Query(blog.schema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
 
 
-class Mutation(t_schema.Mutation, graphene.ObjectType):
+class Mutation(blog.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
